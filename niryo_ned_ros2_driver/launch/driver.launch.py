ef7be3fd5fc7cfc8d82b9a5f03e7a9512deca417
Thomas Degallaix <t.degallaix@niryo.com>
2025-03-24 16:27:39 +0100

    feat: add driver launchfile

---
+++ niryo_ned_ros2_driver/launch/driver.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    default_config_path = os.path.join(
+        get_package_share_directory("niryo_ned_ros2_driver"), "config", "default.yaml"
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package="niryo_ned_ros2_driver",
+                executable="ros2_driver",
+                name="ros2_driver",
+                parameters=[default_config_path],
+            )
+        ]
+    )

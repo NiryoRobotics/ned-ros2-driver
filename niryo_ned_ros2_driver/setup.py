5d57e03ee6098ad63d05a311637ae804e90d64cf
Thomas Degallaix <t.degallaix@niryo.com>
2025-03-24 18:27:02 +0100

    refactor: change file names

--- niryo_ned_ros2_driver/setup.py
+++ niryo_ned_ros2_driver/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=["pytest"],
     entry_points={
         "console_scripts": [
-            "ros2_driver = niryo_ned_ros2_driver.driver:main",
+            "ros2_driver = niryo_ned_ros2_driver.bridge_node:main",
         ],
     },
 )

dd6f0739bce7c767fd6bea558a5746b2cdcbfadf
Thomas Degallaix <t.degallaix@niryo.com>
2025-03-31 17:52:55 +0200

    feat: add ROSTypes dataclass repr

--- niryo_ned_ros2_driver/niryo_ned_ros2_driver/models.py
+++ niryo_ned_ros2_driver/niryo_ned_ros2_driver/models.py
@@ -5,3 +5,6 @@ from dataclasses import dataclass
 class ROSTypes:
     ros1_type: str
     ros2_type: str
+
+    def __repr__(self):
+        return f"ROS1 Type: {self.ros1_type}, ROS2 Type: {self.ros2_type}"
